9c8c46dc98d92431bccf47626768dc798418d603
notbryant <notbryant@gmail.com>
2015-07-28 22:18:28 -0600

    Got to functioning conversion
    TODO: deleting; actually using the sync config for selection

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
 # encoding: utf-8
 import json, os
 from tinytag import TinyTag
-import pprint
-
-pp = pprint.PrettyPrinter(indent=4)
-
-MEDIA_DIR = "/run/media/notbryant/5688b375-0c10-45d5-9e82-52acb0060cf6/Media"
-os.chdir(MEDIA_DIR)
-
-TEST_DIR = 'Music/Owl City/Ocean Eyes'
-TEST_FILE = '01 - Cave In.flac'
-
-tag = TinyTag.get(os.path.join(MEDIA_DIR, TEST_DIR, TEST_FILE))
-print('This track is by %s.' % tag.artist)
-print('It is %f seconds long.' % tag.duration)
-
-
-
-
-#with open('sync.json') as data_file:    
-#    syncList = json.load(data_file)
-
-def printj(j):
-    print(json.dumps(j, sort_keys=True, indent=4, separators=(',', ': ')))
+import subprocess
+import shutil
 
 
 class DirListing:
@@ -48,35 +28,91 @@ class DirListing:
     def minus(self, other):
         return self._min(self.fileList, other.fileList)
     
+    def altered(self, other):
+        return self._alt(self.fileList, other.fileList)
+    
+    def _alt(self, myList, otherList):
+        result = {}
+        for key in myList:
+            if key == ".":
+                for basename in myList[key]:
+                    if key in otherList and basename in otherList[key] and myList[key][basename] != otherList[key][basename]:
+                        result.setdefault(key,{})[basename] = myList[key][basename]
+            elif isinstance(myList[key], dict):
+                if key in otherList:
+                    result[key] = self._alt(myList[key], otherList[key])
+        return result
+    
     def _min(self, myList, otherList):
         result = {}
         for key in myList:
             if key == ".":
-                result[key] = {}
                 for basename in myList[key]:
-                    if key not in otherList or basename not in otherList[key] or myList[key][basename] != otherList[key][basename]:
-                        result[key][basename] = myList[key][basename]
-            elif key not in otherList:
-                if isinstance(myList[key], dict):
-                    result[key] = self._min(myList[key], otherList[key] if key in otherList else {})
+                    if key not in otherList or basename not in otherList[key]:
+                        result.setdefault(key,{})[basename] = myList[key][basename]
+            elif isinstance(myList[key], dict):
+                if key in otherList:
+                    result[key] = self._min(myList[key], otherList[key])
                 else:
                     result[key] = myList[key]
         return result
-        result = {}
-        for key in otherList:
-            if isinstance(otherList[key], dict):
-                result[key] = self._cmpDeleted(otherList[key])
-            else:
-                result[key] = "-"
-        return result
+
+
+def addFiles(fileList, path=""):
+    if "." in fileList:
+        srcDir = os.path.join(source.dirPath, path)
+        destDir = os.path.join(dest.dirPath, path)
+        if not os.path.isdir(destDir):
+            os.makedirs(destDir)
+        for name, ext in fileList["."].items():
+            print(path, name, ext)
+            if ext.lower() != ".flac":
+                shutil.copy2(
+                    os.path.join(srcDir, name + ext),
+                    os.path.join(destDir, name + ext))
+                continue
+            cmd = ['ffmpeg',
+                   '-i', os.path.join(srcDir, name + ext),
+                   '-q:a', '2',
+                   '-map_metadata', '0',
+                   '-id3v2_version', '3',
+                   os.path.join(destDir, name + ".mp3")]
+            print(" ".join(cmd))
+            processes.add(subprocess.Popen(cmd))
+            if len(processes) >= max_processes:
+                os.wait()
+                processes.difference_update([p for p in processes if p.poll() is not None])
+                
+            ## ffmpeg -i "${srcDir}/${name}${ext}" -qscale:a 0 "${destDir}/${name}.mp3"
+    for name in fileList:
+        if name == ".":
+            continue
+        addFiles(fileList[name], os.path.join(path, name))
+
+
+def printj(j):
+    print(json.dumps(j, sort_keys=True, indent=4, separators=(',', ': ')))
+    
+################################################
+
+with open('/run/media/notbryant/5688b375-0c10-45d5-9e82-52acb0060cf6/Media/sync.json') as data_file:
+    syncConfig = json.load(data_file)
+
+
+source = DirListing(syncConfig['sourceDir'])
+dest = DirListing(syncConfig['destDir'])
+
+added = source.minus(dest)
+removed = dest.minus(source)
+#altered = source.altered(dest)
+
+processes = set()
+max_processes = 10
+    
+addFiles(added)
+for p in processes:
+    if p.poll() is None:
+        p.wait()
 
 
 
-
-testA = DirListing(os.path.join(MEDIA_DIR, "Music/Switchfoot"))
-testB = DirListing("/mnt/Tera/Media/Music/Switchfoot")
-#testB = DirListing(os.path.join(MEDIA_DIR, "Music/Owl City/Ocean Eyes"))
-#print(testA)
-#printj(testA.diff(testB))
-printj(testA.minus(testB))
-printj(testB.minus(testA))
